613074c64461f53f18bda566ba3f2956e000277e
dante <are2341@nate.com>
2020-09-09 03:12:36 +0900

    [이상동] 안드로이드 플러그인 갱신

---
+++ Android/app/src/main/java/dante/distribution/android/Global/Define/KGDefine.java
@@ -0,0 +1,58 @@
+package dante.distribution.android.Global.Define;
+
+import android.os.Build;
+
+//! 전역 상수
+public abstract class KGDefine {
+	public static final String TAG = "Plugin";
+	public static final String EMPTY_STRING = "";
+	public static final String INVALID_ANDROID_ID = "9774d56d682e549c";
+	
+	// 버전
+	public static final int MIN_VERSION_APP_UPDATE_MANAGER = Build.VERSION_CODES.LOLLIPOP;
+	public static final int MIN_VERSION_FEEDBACK_GENERATOR = Build.VERSION_CODES.O;
+	
+	// 비율
+	public static final float SCALE_PROGRESS_BAR = 0.25f;
+	public static final float SCALE_PROGRESS_BAR_OFFSET = 0.05f;
+	
+	// 결과
+	public static final String RESULT_TRUE = "True";
+	public static final String RESULT_FALSE = "False";
+	
+	// 빌드 모드
+	public static final String BUILD_MODE_DEBUG = "Debug";
+	public static final String BUILD_MODE_RELEASE = "Release";
+	
+	// 명령어
+	public static final String CMD_GET_DEVICE_ID = "GetDeviceID";
+	public static final String CMD_GET_COUNTRY_CODE = "GetCountryCode";
+	public static final String CMD_GET_STORE_VERSION = "GetStoreVersion";
+	public static final String CMD_SET_BUILD_MODE = "SetBuildMode";
+	public static final String CMD_SHOW_ALERT = "ShowAlert";
+	public static final String CMD_SHOW_TOAST = "ShowToast";
+	public static final String CMD_VIBRATE = "Vibrate";
+	public static final String CMD_ACTIVITY_INDICATOR = "ActivityIndicator";
+	
+	// 키 {
+	public static final String KEY_CMD = "Cmd";
+	public static final String KEY_MSG = "Msg";
+	
+	public static final String KEY_VERSION = "Version";
+	
+	public static final String KEY_ALERT_TITLE = "Title";
+	public static final String KEY_ALERT_MSG = "Msg";
+	public static final String KEY_ALERT_OK_BTN_TEXT = "OKBtnText";
+	public static final String KEY_ALERT_CANCEL_BTN_TEXT = "CancelBtnText";
+	
+	public static final String KEY_VIBRATE_DURATION = "Duration";
+	public static final String KEY_VIBRATE_INTENSITY = "Intensity";
+	
+	public static final String KEY_DEVICE_MS_RESULT = "Result";
+	public static final String KEY_DEVICE_MS_VERSION = "Version";
+	// 키 }
+	
+	// 이름
+	public static final String OBJ_NAME_DEVICE_MSG_RECEIVER = "CDeviceMsgReceiver";
+	public static final String FUNC_NAME_DEVICE_MSG_HANDLER = "HandleDeviceMsg";
+}
---
+++ Android/app/src/main/java/dante/distribution/android/Global/Function/GlobalFunc.java
@@ -0,0 +1,21 @@
+package dante.distribution.android.Global.Function;
+
+import dante.distribution.android.Global.Define.KGDefine;
+
+//! 전역 함수
+public abstract class GlobalFunc {
+	//! 문자열 유효 여부를 검사한다
+	public static boolean isValid(String a_oString) {
+		return a_oString != null && a_oString.length() >= 1;
+	}
+	
+	//! 문자열 -> 논리로 변환한다
+	public static boolean convertStringToBool(String a_oString) {
+		return a_oString.equals(KGDefine.RESULT_TRUE);
+	}
+	
+	//! 논리 -> 문자열로 변환한다
+	public static String convertBoolToString(boolean a_bIsTrue) {
+		return a_bIsTrue ? KGDefine.RESULT_TRUE : KGDefine.RESULT_FALSE;
+	}
+}

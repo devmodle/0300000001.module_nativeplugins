7061be236d853cff63575f7da2f0e1d3e767fa8c
dante <are2341@nate.com>
2021-07-31 21:29:13 +0900

    [이상동] 안드로이드 프로젝트 생성

---
+++ Android/unityLibrary/build/generated/source/buildConfig/release/dante/distribution/android/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package dante.distribution.android;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String LIBRARY_PACKAGE_NAME = "dante.distribution.android";
+  public static final String BUILD_TYPE = "release";
+  public static final int VERSION_CODE = -1;
+  public static final String VERSION_NAME = "";
+}

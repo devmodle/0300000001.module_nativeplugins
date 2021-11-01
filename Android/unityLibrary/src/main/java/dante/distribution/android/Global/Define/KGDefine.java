e85d8bb9f379244cd8463a4ab59675a42686d64d
dante <are2341@nate.com>
2021-11-01 11:57:38 +0900

    [이상동] 안드로이드 프로젝트 추가

---
+++ Android/unityLibrary/src/main/java/dante/distribution/android/Global/Define/KGDefine.java
@@ -0,0 +1,87 @@
+package dante.distribution.android.Global.Define;
+
+import android.os.Build;
+
+/** 전역 상수 */
+public abstract class KGDefine {
+	// 기타 {
+	public static final String TAG = "Plugin";
+	public static final String EMPTY_STR = "";
+	public static final String INVALID_ANDROID_ID = "9774d56d682e549c";
+	
+	public static final String MAIL_DATA = "mailto:";
+	public static final String MAIL_TYPE = "message/rfc822";
+	// 기타 }
+	
+	// 값 {
+	public static final int VAL_0_INT = 0;
+	public static final int VAL_1_INT = 1;
+	public static final int VAL_2_INT = 2;
+	public static final int VAL_3_INT = 3;
+	public static final int VAL_4_INT = 4;
+	public static final int VAL_5_INT = 5;
+	public static final int VAL_6_INT = 6;
+	public static final int VAL_7_INT = 7;
+	public static final int VAL_8_INT = 8;
+	public static final int VAL_9_INT = 9;
+	
+	public static final float VAL_0_FLT = 0.0f;
+	public static final float VAL_1_FLT = 1.0f;
+	public static final float VAL_2_FLT = 2.0f;
+	public static final float VAL_3_FLT = 3.0f;
+	public static final float VAL_4_FLT = 4.0f;
+	public static final float VAL_5_FLT = 5.0f;
+	public static final float VAL_6_FLT = 6.0f;
+	public static final float VAL_7_FLT = 7.0f;
+	public static final float VAL_8_FLT = 8.0f;
+	public static final float VAL_9_FLT = 9.0f;
+	// 값 }
+	
+	// 유닛
+	public static final int UNIT_SEC_TO_MILLISEC = 1000;
+	public static final int UNIT_NORM_VAL_TO_BYTE = 255;
+	
+	// 버전
+	public static final int MIN_VER_FEEDBACK_GENERATOR = Build.VERSION_CODES.O;
+	
+	// 비율
+	public static final float SCALE_PROGRESS_BAR = 0.25f;
+	public static final float OFFSET_SCALE_PROGRESS_BAR = 0.05f;
+	
+	// 결과
+	public static final String RESULT_TRUE = "True";
+	public static final String RESULT_FALSE = "False";
+	
+	// 명령어 {
+	public static final String CMD_GET_DEVICE_ID = "GetDeviceID";
+	public static final String CMD_GET_COUNTRY_CODE = "GetCountryCode";
+	
+	public static final String CMD_SHOW_ALERT = "ShowAlert";
+	public static final String CMD_SHOW_TOAST = "ShowToast";
+	
+	public static final String CMD_MAIL = "Mail";
+	public static final String CMD_VIBRATE = "Vibrate";
+	public static final String CMD_INDICATOR = "Indicator";
+	// 명령어 }
+	
+	// 식별자 {
+	public static final String KEY_CMD = "Cmd";
+	public static final String KEY_MSG = "Msg";
+
+	public static final String KEY_ALERT_TITLE = "Title";
+	public static final String KEY_ALERT_MSG = "Msg";
+	public static final String KEY_ALERT_OK_BTN_TEXT = "OKBtnText";
+	public static final String KEY_ALERT_CANCEL_BTN_TEXT = "CancelBtnText";
+	
+	public static final String KEY_MAIL_RECIPIENT = "Recipient";
+	public static final String KEY_MAIL_TITLE = "Title";
+	public static final String KEY_MAIL_MSG = "Msg";
+	
+	public static final String KEY_VIBRATE_DURATION = "Duration";
+	public static final String KEY_VIBRATE_INTENSITY = "Intensity";
+	// 식별자 }
+	
+	// 이름
+	public static final String OBJ_N_DEVICE_MSG_RECEIVER = "CDeviceMsgReceiver";
+	public static final String FUNC_N_DEVICE_MSG_HANDLER = "HandleDeviceMsg";
+}
---
+++ Android/unityLibrary/src/main/java/dante/distribution/android/Global/Utility/Platform/CDeviceMsgSender.java
@@ -0,0 +1,71 @@
+package dante.distribution.android.Global.Utility.Platform;
+
+import android.util.Log;
+
+import com.unity3d.player.UnityPlayer;
+
+import org.json.JSONObject;
+
+import dante.distribution.android.Global.Define.KGDefine;
+import dante.distribution.android.Global.Function.GFunc;
+
+/** 디바이스 메세지 전송자 */
+public class CDeviceMsgSender {
+	private static CDeviceMsgSender m_oInst = null;
+	
+	/** 생성자 */
+	private CDeviceMsgSender() {
+		// Do Something
+	}
+	
+	/** 인스턴스를 반환한다 */
+	public static CDeviceMsgSender getInst() {
+		// 인스턴스가 없을 경우
+		if(CDeviceMsgSender.m_oInst == null) {
+			CDeviceMsgSender.m_oInst = new CDeviceMsgSender();
+		}
+		
+		return CDeviceMsgSender.m_oInst;
+	}
+	
+	/** 디바이스 식별자 반환 메세지를 전송한다 */
+	public void sendGetDeviceIDMsg(String a_oDeviceID) {
+		try {
+			this.sendDeviceMsg(KGDefine.CMD_GET_DEVICE_ID, a_oDeviceID);
+		} catch(Exception oException) {
+			Log.e(KGDefine.TAG, String.format("CDeviceMsgSender.sendGetDeviceIDMsg Exception: %s, %s", a_oDeviceID, oException.getMessage()));
+			oException.printStackTrace();
+		}
+	}
+	
+	/** 국가 코드 반환 메세지를 전송한다 */
+	public void sendGetCountryCodeMsg(String a_oCountryCode) {
+		try {
+			this.sendDeviceMsg(KGDefine.CMD_GET_COUNTRY_CODE, a_oCountryCode);
+		} catch(Exception oException) {
+			Log.e(KGDefine.TAG, String.format("CDeviceMsgSender.sendGetCountryCodeMsg Exception: %s, %s", a_oCountryCode, oException.getMessage()));
+			oException.printStackTrace();
+		}
+	}
+	
+	/** 경고 창 출력 메세지를 전송한다 */
+	public void sendShowAlertMsg(boolean a_bIsOK) {
+		String oResult = GFunc.convertBoolToStr(a_bIsOK);
+		
+		try {
+			this.sendDeviceMsg(KGDefine.CMD_SHOW_ALERT, oResult);
+		} catch(Exception oException) {
+			Log.e(KGDefine.TAG, String.format("CDeviceMsgSender.sendShowAlertMsg Exception: %s, %s", oResult, oException.getMessage()));
+			oException.printStackTrace();
+		}
+	}
+	
+	/** 디바이스 메세지를 전송한다 */
+	private void sendDeviceMsg(String a_oCmd, String a_oMsg) throws Exception {
+		JSONObject oJSONObj = new JSONObject();
+		oJSONObj.put(KGDefine.KEY_CMD, a_oCmd);
+		oJSONObj.put(KGDefine.KEY_MSG, a_oMsg);
+		
+		UnityPlayer.UnitySendMessage(KGDefine.OBJ_N_DEVICE_MSG_RECEIVER, KGDefine.FUNC_N_DEVICE_MSG_HANDLER, oJSONObj.toString());
+	}
+}
